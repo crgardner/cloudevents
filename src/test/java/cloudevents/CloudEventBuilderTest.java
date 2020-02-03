47cd1092c9ebf5d5651b8736a4a2816f72f0e374
cgardner <chris.r.gardner@gmail.com>
2020-02-02 21:38:29 -0500

    Initial import

---
+++ src/test/java/cloudevents/CloudEventBuilderTest.java
@@ -0,0 +1,42 @@
+package cloudevents;
+
+import io.cloudevents.json.Json;
+import io.cloudevents.v03.CloudEventBuilder;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.net.URI;
+
+public class CloudEventBuilderTest {
+    static class MyData {
+        private final String name;
+        private final String value;
+
+        MyData(String name, String value) {
+            this.name = name;
+            this.value = value;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public String getValue() {
+            return value;
+        }
+    }
+
+
+    @Test
+    void hasSource() {
+        var myData = new MyData("foo", "bar");
+        var event = CloudEventBuilder.<MyData>builder().withId("12345")
+                                     .withSource(URI.create("/source"))
+                                     .withType("myType")
+                                     .withData(myData)
+                                     .build();
+
+        var encodedEvent = Json.encode(event);
+        Assertions.assertThat(event).isNotNull();
+    }
+}
